b694d115785fe7ecc638fc59fbbc46a170552f54
Ashu0126 <ashish.jaryal14@gmail.com>
2024-02-27 11:58:01 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="NetArtApp">
+      <img className="logo" src="/imgs/logo.png" alt="logo" />
+      <div className="main">
+        <img className="trophy" src="/imgs/1.png" alt="trophy" />
+        <div className="mainContent">
+          <h5>
+            C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for
+            the 4th time.
+          </h5>
+          <ul>
+            <li>
+              C.R.I.'s energy efficient products are well recognized by various
+              Government Institutions, as trustworthy products for various
+              projects across the globe to save energy.
+            </li>
+            <li>
+              C.R.I. is the highest contributor in the country for the projects
+              of EESL (Energy Efficiency Services Limited) to replace the old
+              inefficient pumps with 5 Star rated energy efficient smart pumps
+              with IoT enabled control panel.
+            </li>
+          </ul>
+          <img src="/imgs/2.png" alt="main-img" />
+          <p className="caption">
+            Government of India has awarded the
+            <strong> "National Energy Conservation Award 2018"</strong>. Mr. G.
+            Selvaraj, Joint Managing Director of C.R.I. Group received the award
+            from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar
+            Singh, Honorable Minister of State.
+          </p>
+        </div>
+      </div>
+      <div className="product">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY
+          RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF
+          POWER FOR THE NATION.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="productSection">
+          <img src="/imgs/3.png" alt="products" />
+          <p>
+            Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables -
+            Solar Systems - Motors
+          </p>
+        </div>
+      </div>
+      <hr />
+      <footer>
+        <div className="footerContent">
+          <h5>C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</h5>
+          <p>
+            CHEMICALS & PROCESS | POWER | WATER & WASTE WATER | OILS & GAS
+            PHARMA | SUGARS & DISTILLERIES | PAPER & PULP | MARINE & DEFENCE |
+            METAL & MINING | FOOD & BEVERAGE | PETROCHEMICAL & REFINERIES |
+            SOLAR | BUILDING | HVAC | FIRE FIGHTING | AGRICULTURE & RESIDENTIAL
+          </p>
+        </div>
+        <div className="contact">
+          <a href="tel:18002001234">
+            <img src="/imgs/call.png" alt="call" />
+            <span>
+              Toll free <strong>1800 200 1234</strong>
+            </span>
+          </a>
+          <a href="https://www.facebook.com/cripumps">
+            <img src="/imgs/fb.png" alt="fb" />
+            www.facebook.com/cripumps
+          </a>
+          <a href="https://www.crigroups.com/">
+            <img src="/imgs/web.png" alt="web" />
+            www.crigroups.com
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
